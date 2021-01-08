3476a9830d17813cc9cba848eaa61604d3ca42af
MaxvanderWeide <max.vanderweide@student.hu.nl>
2021-01-08 12:08:07 +0100

    Added AccessSecretVersionRequest class to secretmanager DAT-5297

--- functions/create_ticket/secretmanager.py
+++ functions/create_ticket/secretmanager.py
@@ -1,4 +1,5 @@
 from google.cloud import secretmanager_v1
+from google.cloud.secretmanager_v1 import AccessSecretVersionRequest
 
 
 def get_secret(project_id, secret_id):
@@ -8,10 +9,7 @@ def get_secret(project_id, secret_id):
 
     client = secretmanager_v1.SecretManagerServiceClient()
 
-    secret_name = client.secret_version_path(
-        project_id,
-        secret_id,
-        'latest')
+    secret_name = AccessSecretVersionRequest(client.secret_version_path(project_id, secret_id, 'latest'))
 
     response = client.access_secret_version(secret_name)
     payload = response.payload.data.decode('utf-8')
