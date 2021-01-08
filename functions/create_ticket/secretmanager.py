7ba04107dc5d32cb61a0b58d39ad60bee1f88852
MaxvanderWeide <max.vanderweide@student.hu.nl>
2021-01-08 12:33:32 +0100

    Reverted to working secret manager version DAT-5297

--- functions/create_ticket/secretmanager.py
+++ functions/create_ticket/secretmanager.py
@@ -1,5 +1,4 @@
 from google.cloud import secretmanager_v1
-from google.cloud.secretmanager_v1 import AccessSecretVersionRequest
 
 
 def get_secret(project_id, secret_id):
@@ -9,7 +8,7 @@ def get_secret(project_id, secret_id):
 
     client = secretmanager_v1.SecretManagerServiceClient()
 
-    secret_name = AccessSecretVersionRequest(client.secret_version_path(project_id, secret_id, 'latest'))
+    secret_name = client.secret_version_path(project_id, secret_id, 'latest')
 
     response = client.access_secret_version(secret_name)
     payload = response.payload.data.decode('utf-8')
